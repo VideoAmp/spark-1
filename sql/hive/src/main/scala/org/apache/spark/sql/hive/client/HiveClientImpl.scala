1a90f4de4e2a35e7d69adb1cd8696bfe67918d39
Michael Allman <michael@videoamp.com>
2017-01-24 15:24:52 -0800

    [SPARK-18202][SQL] Remove limit on Hive command results

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -692,11 +692,7 @@ private[hive] class HiveClientImpl(
    * Runs the specified SQL query using Hive.
    */
   override def runSqlHive(sql: String): Seq[String] = {
-    val maxResults = 100000
-    val results = runHive(sql, maxResults)
-    // It is very confusing when you only get back some of the results...
-    if (results.size == maxResults) sys.error("RESULTS POSSIBLY TRUNCATED")
-    results
+    runHive(sql, Int.MaxValue)
   }
 
   /**
